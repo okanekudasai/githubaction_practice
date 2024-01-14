f31b5fc7502388d35e3b6ad5bf17b143d8808baa
okane <okanekudasai6@gmail.com>
2024-01-15 04:46:12 +0900

    11ff22333d52

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,21 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @RestController
 public class Demo1Application {
 
+    @Value("build.confirm")
+    String key;
     @GetMapping("/test")
     String test() {
-        return "hello worl";
+        return key;
     }
 
 
+
     public static void main(String[] args) {
+        String[] SCOPES = { "sjeijfseojseifjeifjsieofjeisef" };
+        System.out.println(Arrays.toString(SCOPES));
         SpringApplication.run(Demo1Application.class, args);
     }
 
